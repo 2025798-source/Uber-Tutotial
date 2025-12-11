b84af3af2187e13d3daedaa7c8579e96cee0912b
2025798-source <2025798@student.cct.ie>
2025-12-11 12:37:33 +0000

    map

--- Uber_StreamLit.py
+++ Uber_StreamLit.py
@@ -30,4 +30,7 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins = 24, range = (0,24))
 st.bar_chart(hist_values)
     
 hour_to_filter = st.slider("hour", 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]    
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader("Map of all Pickups at %s:00" % hour_to_filter)
+st.map(filtered_data)    
